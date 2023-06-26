e353513be56925b2d6a0fea1fa6b88824bde5055
dimpram101 <dimas2004211@gmail.com>
2023-06-26 22:06:44 +0800

    fix: remove console log

--- resources/js/Pages/Dashboard/Profile.tsx
+++ resources/js/Pages/Dashboard/Profile.tsx
@@ -29,7 +29,6 @@ const Profile = ({ user, roles }: Props) => {
 
   const onSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(form.data);
     form.put(route('user.update', user.id), {
       onFinish: () => form.reset('password', 'confirm_password')
     });
